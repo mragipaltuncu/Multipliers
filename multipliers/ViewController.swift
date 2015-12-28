b23147ee823a65a7c585d9b63d4eff0d1d183f55
Mehmet Ragıp Altuncu <mragipaltuncu@Mehmet-MacBook-Pro.local>
2015-12-27 21:17:50 -0800

    multiply game done

--- multipliers/ViewController.swift
+++ multipliers/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var addByNumber = 0
+    var totalNumber = 0
+    
 
+    @IBOutlet weak var titleImage: UIImageView!
+    @IBOutlet weak var addLbl: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addByText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func gameStarted() {
+        titleImage.hidden = true
+        playButton.hidden = true
+        addByText.hidden = true
+        
+        addButton.hidden = false
+        addLbl.hidden = false
+        
+    }
 
+    func gameEnds() {
+        
+        addByText.text = ""
+        
+        titleImage.hidden = false
+        playButton.hidden = false
+        addByText.hidden = false
+        
+        addButton.hidden = true
+        addLbl.hidden = true
+        
+    }
+    @IBAction func playButtonPressed(sender: UIButton) {
+        
+        gameStarted()
+        addByNumber = Int(addByText.text!)!
+    }
+    
+    @IBAction func addButtonPressed(sender: UIButton) {
+        
+        addLbl.text = "\(totalNumber) + \(addByNumber) = \(totalNumber + addByNumber)"
+        totalNumber += addByNumber
+        if totalNumber >= 20 * addByNumber {
+            gameEnds()
+        }
+        
+    }
 
 }
 
